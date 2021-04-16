a803f26f445c5700e77e42d8b896995e16176208
xenups <xenups@outlook.com>
2021-04-17 03:10:08 +0430

    Fixed issue addressed int cannt allocate to prePaintWindow

--- shapecorners.cpp
+++ shapecorners.cpp
@@ -195,7 +195,7 @@ ShapeCornersEffect::prePaintWindow(KWin::EffectWindow *w, KWin::WindowPrePaintDa
 	    || (w == applyEffect)
             )
     {
-        KWin::effects->prePaintWindow(w, data, time);
+        KWin::effects->prePaintWindow(w, data, std::chrono::milliseconds(time));
         return;
     }
     const QRect geo(w->geometry());
@@ -215,7 +215,7 @@ ShapeCornersEffect::prePaintWindow(KWin::EffectWindow *w, KWin::WindowPrePaintDa
     outerRect += QRegion(geo.x()+m_size, geo.y(), geo.width()-m_size*2, 1);
     data.paint += outerRect;
     data.clip -=outerRect;
-    KWin::effects->prePaintWindow(w, data, time);
+    KWin::effects->prePaintWindow(w, data, std::chrono::milliseconds(time));
 }
 
 static bool hasShadow(KWin::WindowQuadList &qds)
